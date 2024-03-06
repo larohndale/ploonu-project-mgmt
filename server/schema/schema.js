bdc3cec3c9423937b3a85d4d61cea34d248fa17f
Dominque LaRohn Dale Cox <larohndale@gmail.com>
2024-03-06 14:52:57 -0500

    adding mutations for clients in graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,7 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = require('graphql');
 
 // Project Type
@@ -76,7 +77,43 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+// Mutations
+const clientMutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    // Delete Client
+    addClient: {
+      type: ClientType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const client = new Client({
+          name: args.name,
+          email: args.email,
+          phone: args.phone,
+        });
+
+        return client.save();
+      },
+    },
+    // Delete Client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Client.findByIdAndDelete(args.id);
+      },
+    },
+  },
+});
+
 // Export the query to use it later (in the index.js)
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: clientMutation,
 });
