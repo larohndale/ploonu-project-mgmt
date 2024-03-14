e5465ad3eff4e97da4dd96d5c9613dd81da0f4dc
Dominque LaRohn Dale Cox <larohndale@gmail.com>
2024-03-14 10:02:40 -0400

    created projects page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
+
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 // Handle cache merging
@@ -38,6 +42,8 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </Router>
