ec7ceb3313463926fb7a26b8938ea13e4ec4267c
Dominque LaRohn Dale Cox <larohndale@gmail.com>
2024-03-22 09:39:29 -0400

    wrap up base impl

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,24 +2,24 @@ import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientQueries';
-
+import { GET_PROJECTS } from '../queries/projectQueries';
 export default function ClientRow({ client }) {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: {
       id: client.id,
     },
     // Refresh client
-    // refetchQueries: [{ query: GET_CLIENTS }],
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
     // Refresh via updating cache
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    //   update(cache, { data: { deleteClient } }) {
+    //     const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //     cache.writeQuery({
+    //       query: GET_CLIENTS,
+    //       data: {
+    //         clients: clients.filter((client) => client.id !== deleteClient.id),
+    //       },
+    //     });
+    //   },
   });
 
   return (
